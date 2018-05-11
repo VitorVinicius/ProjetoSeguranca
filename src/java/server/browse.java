0f8b40ddd54328a2d434a6e17de26de2a5d4cb2c
VitorVinicius <vitor.v.gomes@live.com>
2018-05-11 01:03:24 -0300

    Commit Inicial

---
+++ src/java/server/browse.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ *
+ * @author Vitor
+ */
+@WebServlet(name = "browse", urlPatterns = {"/browse"})
+public class browse extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            out.println("          <table id=\"FileList\" style=\"width:100%\">");
+            out.println("<thead>");
+            out.println("  <tr>");
+            out.println("    <th>Filename</th>");
+            out.println("    <th>Size</th> ");
+            out.println("  </tr>");
+            out.println("</thead>");
+            out.println("<tbody>");
+            String folderPath = "c:\\arquivos";
+            File folder = new File(folderPath);
+            if(!folder.exists()){
+                folder.mkdir();
+            }
+            File[] listOfFiles = folder.listFiles();
+
+            for (File file : listOfFiles) {
+                if (file.isFile()) {
+                    out.println("  <tr>");
+                    out.println("    <td><a href=\"/download?file="+file.getName()+"\">"+file.getName()+"</a></td>");
+                    out.println("    <td>"+(file.length()/1000)+"KB</td> ");
+                    out.println("  </tr>");
+                    
+                }
+            }
+            
+            
+            
+            
+            out.println("</tbody>");
+            out.println("</table>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
